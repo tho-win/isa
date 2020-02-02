294c0d2511a8de875ad1478d9c1810b6eb9fbde2
tho-win <tnn7yc@virginia.edu>
2020-02-02 12:40:13 -0500

    added Student, School models

--- app/foo/swipeme/models.py
+++ app/foo/swipeme/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=300)
+    street_address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    pass
+
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    swiper = models.BooleanField(default=False)
+    schools = models.ManyToManyField(School)
