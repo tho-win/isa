c8d03c910cbad24344a0fb45672ce211ce4afb1b
tho-win <tnn7yc@virginia.edu>
2020-02-08 12:00:17 -0500

    added .gitignore

--- app/foo/api/urls.py
+++ app/foo/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import *
+from api import views
 
 router = routers.DefaultRouter()
 router.register(r'user', CustomUserViewSet)
@@ -9,5 +10,6 @@ router.register(r'school', SchoolViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    #path('user/<int:pk>/', views.user_detail_by_id),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- app/foo/api/views.py
+++ app/foo/api/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 from .serializers import *
 from users import models as users_models
 from swipeme import models as swipeme_models
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = users_models.CustomUser.objects.all().order_by('username')
     serializer_class = CustomUserSerializer
 
-
 class ProfileViewSet(viewsets.ModelViewSet):
 	queryset = users_models.Profile.objects.all().order_by('user')
 	serializer_class = ProfileSerializer
-
 
 class SchoolViewSet(viewsets.ModelViewSet):
 	queryset = swipeme_models.School.objects.all().order_by('name')
