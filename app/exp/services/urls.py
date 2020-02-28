5db9f1af94b3292c0250acfb67c67a93882ce9bd
tho-win <tnn7yc@virginia.edu>
2020-02-27 22:10:21 -0500

    added homepage, show all users and user detail basic layouts and logic in web and exp layers

--- app/exp/services/urls.py
+++ app/exp/services/urls.py
@@ -1,9 +1,8 @@
-from . import views
-from django.urls import path, include
+from django.urls import path
 
-app_name = 'frontend'
+from . import views
+
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
     path('users/', views.show_all_users, name='show_all_users'),
     path('users/<int:uid>/', views.user_detail, name='user_detail'),
 ]
