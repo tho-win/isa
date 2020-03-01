84f0f0e4aa28b271219bf715869fc2c2704ee9e9
Haoran Zhu <hz3fr@virginia.edu>
2020-02-29 21:56:19 -0500

    Add homepage, details page and Post model

--- app/exp/services/urls.py
+++ app/exp/services/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('posts/', views.show_all_posts, name='show_all_posts'),
     path('users/', views.show_all_users, name='show_all_users'),
     path('users/<int:uid>/', views.user_detail, name='user_detail'),
 ]
--- app/web/frontend/urls.py
+++ app/web/frontend/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 app_name = 'frontend'
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('posts/', views.show_all_posts, name="show_all_posts"),
+  	path('specialposts', views.show_special_posts, name="show_special_posts"),
     path('users/', views.show_all_users, name='show_all_users'),
     path('users/<int:uid>/', views.user_detail, name='user_detail'),
 ]
