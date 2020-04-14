a5edf94da6cfabb1130b9a720865fff59e6f3f4f
Haoran Zhu <hz3fr@virginia.edu>
2020-04-14 12:12:43 -0400

    track user_id

--- app/access_log/post_view.py
+++ app/access_log/post_view.py
@@ -21,12 +21,13 @@ while True:
     else:
         break
 
-with open('access_log.csv', 'a') as file:
-    while True:
-        for message in consumer:
-            new_item = json.loads((message.value).decode('utf-8'))
-            es.update(index='listing_index', doc_type='listing', id=new_item['listing_id'] , body={ 'script' : 'ctx._source.visits += 1'})
-            es.indices.refresh(index="listing_index")
-            entry = str(new_item['user_id']) + "," + str(new_item['listing_id']) + "\n"
-            file.write(entry)
+while True:
+    for message in consumer:
+        new_item = json.loads((message.value).decode('utf-8'))
+        es.update(index='listing_index', doc_type='listing', id=new_item['listing_id'] , body={ 'script' : 'ctx._source.visits += 1'})
+        entry = str(new_item['user_id']) + "," + str(new_item['listing_id']) + "\n"
+        file = open('access_log.csv', 'a')
+        file.write(entry)
+        file.close()
+        es.indices.refresh(index="listing_index")
 
