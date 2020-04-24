6a1fca50e2fbf08e8382e5de97f6e6fac3c326b2
Haoran Zhu <hz3fr@virginia.edu>
2020-04-23 22:34:21 -0400

    integration test#1

--- app/selenium/selenium_test.py
+++ app/selenium/selenium_test.py
@@ -15,29 +15,30 @@ BIO = "I'm a test user.!"
 
 TITLE = "Test post"
 CONTENT = "content"
-PRICE = 2.0
-SWIPES = 10
+PRICE = "8"
+SWIPES = "10"
+PICKUP = "Lambeth Field"
 
-# class DisplayTest(unittest.TestCase):
-# 	def setUp(self):
-# 		# 'http://selenium-chrome:4444/wd/hub'
-# 		self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', 
-# 			desired_capabilities=DesiredCapabilities.CHROME)
+class DisplayTest(unittest.TestCase):
+	def setUp(self):
+		# 'http://selenium-chrome:4444/wd/hub'
+		self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', 
+			desired_capabilities=DesiredCapabilities.CHROME)
 
-# 	def test_homepage(self): 
-# 		self.driver.get("http://64.225.30.56:8000/")
-# 		self.assertIn("Home", self.driver.title)
+	def test_homepage(self): 
+		self.driver.get("http://64.225.30.56:8000/")
+		self.assertIn("Home", self.driver.title)
 
-# 	def test_login(self): 
-# 		self.driver.get("http://64.225.30.56:8000/login/")
-# 		self.assertIn("Log In", self.driver.title)
+	def test_login(self): 
+		self.driver.get("http://64.225.30.56:8000/login/")
+		self.assertIn("Log In", self.driver.title)
 
-# 	def test_signup(self): 
-# 		self.driver.get("http://64.225.30.56:8000/signup/")
-# 		self.assertIn("Sign Up", self.driver.title)
+	def test_signup(self): 
+		self.driver.get("http://64.225.30.56:8000/signup/")
+		self.assertIn("Sign Up", self.driver.title)
 
-# 	def tearDown(self):
-# 		self.driver.close()
+	def tearDown(self):
+		self.driver.close()
 
 class ASignUpTest(unittest.TestCase):
 	def setUp(self):
@@ -139,6 +140,7 @@ class BLogInTest(unittest.TestCase):
 		username.send_keys("sndhvfbnms")
 		password = self.driver.find_element_by_name("password")
 		password.send_keys(PASSWORD)
+		password.submit()
 		self.assertNotIn("Profile", self.driver.page_source)
 		self.assertNotIn("Log out", self.driver.page_source)
 
@@ -148,6 +150,7 @@ class BLogInTest(unittest.TestCase):
 		username.send_keys(USER_NAME)
 		password = self.driver.find_element_by_name("password")
 		password.send_keys("PASSWORD")
+		password.submit()
 		self.assertNotIn("Profile", self.driver.page_source)
 		self.assertNotIn("Log out", self.driver.page_source)
 
@@ -160,12 +163,78 @@ class CSearchTest(unittest.TestCase):
 		self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', 
 			desired_capabilities=DesiredCapabilities.CHROME)
 
+	def test_a_search_success_1(self):
+		self.driver.get("http://64.225.30.56:8000/")
+		search = self.driver.find_element_by_name("search")
+		search.send_keys("newcomb")
+		search.submit()
+		self.assertIn("Search result for \"newcomb\"", self.driver.page_source)
+		self.assertIn("Swipes at Newcomb", self.driver.page_source)
+
+	def test_a_search_success_2(self):
+		self.driver.get("http://64.225.30.56:8000/")
+		search = self.driver.find_element_by_name("search")
+		search.send_keys("plus dollar")
+		search.submit()
+		self.assertIn("Search result for \"plus dollar\"", self.driver.page_source)
+		self.assertIn("Pick up: On grounds only", self.driver.page_source)
+
+	def test_b_search_no_listing(self):
+		self.driver.get("http://64.225.30.56:8000/")
+		search = self.driver.find_element_by_name("search")
+		search.send_keys("liuhcjfsbngh")
+		search.submit()
+		self.assertIn("No listing match \"liuhcjfsbngh\"", self.driver.page_source)
 	
 	def tearDown(self):
 		self.driver.close()	
 
+
+class DCreatePostTest(unittest.TestCase):
+	def setUp(self):
+		self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', 
+			desired_capabilities=DesiredCapabilities.CHROME)
+		self.driver.get("http://64.225.30.56:8000/login/")
+		username = self.driver.find_element_by_name("username")
+		username.send_keys("haoranzhu")
+		password = self.driver.find_element_by_name("password")
+		password.send_keys("zhr1999512")
+		password.submit()
+
+	def fill_out_post_creation(self):
+		title = self.driver.find_element_by_name("title")
+		title.send_keys(TITLE)
+		content = self.driver.find_element_by_name("content")
+		content.send_keys(CONTENT)
+		price = self.driver.find_element_by_name("price")
+		price.send_keys(PRICE)
+		remaining_nums = self.driver.find_element_by_name("remaining_nums")
+		remaining_nums.send_keys(SWIPES)
+		pickup_address = self.driver.find_element_by_name("pickup_address")
+		pickup_address.send_keys(PICKUP)
+		pickup_address.submit()
+
+	def test_a_create_post_success(self):
+		self.driver.get("http://64.225.30.56:8000/create_listing/")
+		self.fill_out_post_creation()
+		self.assertIn("Your post has been created.", self.driver.page_source)
+		self.assertIn(TITLE, self.driver.page_source)
+
+	def test_b_search_created_post(self):
+		time.sleep(5)
+		self.driver.get("http://64.225.30.56:8000/")
+		search = self.driver.find_element_by_name("search")
+		search.send_keys(TITLE)
+		search.submit()
+		self.assertIn("Search result for \"Test post\"", self.driver.page_source)
+		self.assertIn(TITLE, self.driver.page_source)
+
+	def tearDown(self):
+		self.driver.close()	
+
+
+
 if __name__ == "__main__":
 	unittest.main()
-
 
 
