e032ae9f2ef204d7317a914a90131cd71de743c4
Haoran Zhu <hz3fr@virginia.edu>
2020-04-27 16:02:58 -0400

    update integration test#1

--- app/selenium/selenium_test.py
+++ app/selenium/selenium_test.py
@@ -1,14 +1,17 @@
 import unittest
 import time
+import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-USER_NAME = "test_user"
+i = random.randint(10, 999999)
+
+USER_NAME = "test_user" + str(i)
 COMPUTING_ID = "test"
 FIRST_NAME = "Test"
 LAST_NAME = "User"
-EMAIL = COMPUTING_ID + "@virginia.edu"
+EMAIL = COMPUTING_ID + "@virginia.edu" + str(i)
 PASSWORD = "pAsswrd123!!"
 PHONE_NUMBER = "5555555555"
 BIO = "I'm a test user.!"
@@ -26,15 +29,15 @@ class DisplayTest(unittest.TestCase):
 			desired_capabilities=DesiredCapabilities.CHROME)
 
 	def test_homepage(self): 
-		self.driver.get("http://64.225.30.56:8000/")
+		self.driver.get("http://64.225.30.56/")
 		self.assertIn("Home", self.driver.title)
 
 	def test_login(self): 
-		self.driver.get("http://64.225.30.56:8000/login/")
+		self.driver.get("http://64.225.30.56/login/")
 		self.assertIn("Log In", self.driver.title)
 
 	def test_signup(self): 
-		self.driver.get("http://64.225.30.56:8000/signup/")
+		self.driver.get("http://64.225.30.56/signup/")
 		self.assertIn("Sign Up", self.driver.title)
 
 	def tearDown(self):
@@ -65,13 +68,13 @@ class ASignUpTest(unittest.TestCase):
 		bio.submit()
 
 	def test_a_sign_up_success(self):
-		self.driver.get("http://64.225.30.56:8000/signup/")
+		self.driver.get("http://64.225.30.56/signup/")
 		self.fill_out_sign_up()
 		time.sleep(5)
-		self.assertIn("Account created for test_user", self.driver.page_source)
+		self.assertIn("Account created for", self.driver.page_source)
 
 	def test_b_sign_up_email_taken(self):
-		self.driver.get("http://64.225.30.56:8000/signup/")
+		self.driver.get("http://64.225.30.56/signup/")
 		first_name = self.driver.find_element_by_name("first_name")
 		first_name.send_keys(FIRST_NAME)
 		last_name = self.driver.find_element_by_name("last_name")
@@ -92,13 +95,13 @@ class ASignUpTest(unittest.TestCase):
 		self.assertNotIn("Account created for test_user_2", self.driver.page_source)
 
 	def test_c_sign_up_username_taken(self):
-		self.driver.get("http://64.225.30.56:8000/signup/")
+		self.driver.get("http://64.225.30.56/signup/")
 		first_name = self.driver.find_element_by_name("first_name")
 		first_name.send_keys(FIRST_NAME)
 		last_name = self.driver.find_element_by_name("last_name")
 		last_name.send_keys(LAST_NAME)
 		username = self.driver.find_element_by_name("username")
-		username.send_keys("test_user")
+		username.send_keys(USER_NAME)
 		email = self.driver.find_element_by_name("email")
 		email.send_keys("xxxxx@gmail.com")
 		password = self.driver.find_element_by_name("password")
@@ -110,7 +113,7 @@ class ASignUpTest(unittest.TestCase):
 		bio = self.driver.find_element_by_name("bio")
 		bio.send_keys(BIO)
 		bio.submit()
-		self.assertNotIn("Account created for test_user", self.driver.page_source)
+		self.assertNotIn("Account created for " + USER_NAME, self.driver.page_source)
 
 	def tearDown(self):
 		self.driver.close()	
@@ -130,13 +133,13 @@ class BLogInTest(unittest.TestCase):
 
 	def test_a_log_in_success(self):
 		time.sleep(3)
-		self.driver.get("http://64.225.30.56:8000/login/")
+		self.driver.get("http://64.225.30.56/login/")
 		self.fill_out_log_in()
 		self.assertIn("Profile", self.driver.page_source)
 		self.assertIn("Log out", self.driver.page_source)
 
 	def test_b_log_in_username_not_exist(self):
-		self.driver.get("http://64.225.30.56:8000/login/")
+		self.driver.get("http://64.225.30.56/login/")
 		username = self.driver.find_element_by_name("username")
 		username.send_keys("sndhvfbnms")
 		password = self.driver.find_element_by_name("password")
@@ -146,7 +149,7 @@ class BLogInTest(unittest.TestCase):
 		self.assertNotIn("Log out", self.driver.page_source)
 
 	def test_c_log_in_password_incorrect(self):
-		self.driver.get("http://64.225.30.56:8000/login/")
+		self.driver.get("http://64.225.30.56/login/")
 		username = self.driver.find_element_by_name("username")
 		username.send_keys(USER_NAME)
 		password = self.driver.find_element_by_name("password")
@@ -165,7 +168,7 @@ class CSearchTest(unittest.TestCase):
 			desired_capabilities=DesiredCapabilities.CHROME)
 
 	def test_a_search_success_1(self):
-		self.driver.get("http://64.225.30.56:8000/")
+		self.driver.get("http://64.225.30.56/")
 		search = self.driver.find_element_by_name("search")
 		search.send_keys("newcomb")
 		search.submit()
@@ -173,7 +176,7 @@ class CSearchTest(unittest.TestCase):
 		self.assertIn("Swipes at Newcomb", self.driver.page_source)
 
 	def test_a_search_success_2(self):
-		self.driver.get("http://64.225.30.56:8000/")
+		self.driver.get("http://64.225.30.56/")
 		search = self.driver.find_element_by_name("search")
 		search.send_keys("plus dollar")
 		search.submit()
@@ -181,7 +184,7 @@ class CSearchTest(unittest.TestCase):
 		self.assertIn("Pick up: On grounds only", self.driver.page_source)
 
 	def test_b_search_no_listing(self):
-		self.driver.get("http://64.225.30.56:8000/")
+		self.driver.get("http://64.225.30.56/")
 		search = self.driver.find_element_by_name("search")
 		search.send_keys("liuhcjfsbngh")
 		search.submit()
@@ -195,7 +198,7 @@ class DCreatePostTest(unittest.TestCase):
 	def setUp(self):
 		self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', 
 			desired_capabilities=DesiredCapabilities.CHROME)
-		self.driver.get("http://64.225.30.56:8000/login/")
+		self.driver.get("http://64.225.30.56/login/")
 		username = self.driver.find_element_by_name("username")
 		username.send_keys("haoranzhu")
 		password = self.driver.find_element_by_name("password")
@@ -216,14 +219,14 @@ class DCreatePostTest(unittest.TestCase):
 		pickup_address.submit()
 
 	def test_a_create_post_success(self):
-		self.driver.get("http://64.225.30.56:8000/create_listing/")
+		self.driver.get("http://64.225.30.56/create_listing/")
 		self.fill_out_post_creation()
 		self.assertIn("Your post has been created.", self.driver.page_source)
 		self.assertIn(TITLE, self.driver.page_source)
 
 	def test_b_search_created_post(self):
 		time.sleep(5)
-		self.driver.get("http://64.225.30.56:8000/")
+		self.driver.get("http://64.225.30.56/")
 		search = self.driver.find_element_by_name("search")
 		search.send_keys(TITLE)
 		search.submit()
